d929a650a11da18ed9d5220b1a3b3e29512785e3
Sérgio <sergio.r.gama@gmail.com>
2020-10-23 17:39:04 +0100

    Added some new atributes and methods Added PublicStream derivated class from Stream

--- streamz.h
+++ streamz.h
@@ -8,7 +8,7 @@
 using namespace std;
 
 enum Language{
-    PT, EN
+    PT, EN, IT
 };
 
 struct Date {
@@ -17,6 +17,43 @@ struct Date {
     unsigned year;
 };
 
+class Stream;
+
+class User {
+public:
+    string name;
+    Date birthday;
+    Stream *stream;  //or the stream beeing viewed or beeing streammed
+    unsigned min_age;
+    User(string name, Date birthday);
+    ~User();
+};
+
+class Streamer : public User {
+    Streamer();
+    ~Streamer();
+    vector<Stream> history;
+    bool endStream();
+};
+
+Streamer::Streamer() : User(name, birthday){
+    min_age = 15;
+}
+
+bool Streamer::endStream() {  //removes stream from system and return true is success
+}
+
+class Viewer : public User {
+    Viewer();
+    ~Viewer();
+    bool enterStream(Stream *stream);  //can only be in one stream at the time
+    bool exitStream(Stream *stream);  //they can exit at any time
+};
+
+Viewer::Viewer() : User(name , birthday){
+    min_age = 12;
+}
+
 class Stream {
 public:
     Stream();
@@ -24,25 +61,53 @@ public:
     string title;
     Language lang;
     unsigned min_age;
-    vector<Viewer*> viewers;
+    vector<Viewer*> total_viewers;
+    vector<Viewer*> active_viewers;
+    unsigned getTotalViews() const;
+    unsigned getActiveViewers() const;
+};
+
+Stream::Stream() {
+}
+
+Stream::~Stream() {
+}
+
+unsigned Stream::getTotalViews() const {
+    return total_viewers.size();
+}
+
+unsigned Stream::getActiveViewers() const {
+    return active_viewers.size();
+}
+
+class PublicStream : public Stream{
+public:
+    PublicStream();
+    ~PublicStream();
 };
 
 class PrivateStream : public Stream{
 public:
     PrivateStream();
     ~PrivateStream();
-    vector<Viewer*> allowed_viewers;
+    vector<Viewer*> autorized_viewers;
+    vector<string> comments;
     unsigned capacity;
 };
-
 
 class StreamZ {
 public:
     vector<User*> users;
     vector<Stream*> active_streams;
     vector<Stream*> best_streams;
-    Stream* startStream(const Streamer& streamer, string title, Language lang, unsigned min_age);
-    bool stopStream(const Streamer& streamer);
+    Stream* createStream(const Streamer& streamer, string title, Language lang, unsigned min_age);
+    bool removeStream(const Streamer& streamer);
+    StreamZ();
+    ~StreamZ();
 };
+
+StreamZ::StreamZ() {
+}
 
 #endif // STREAMZ_H

d88b9eab8751ef4062295b7bf858afb7b4ead1fa
Lucas Calvet Santos <lucascalvet2000@hotmail.com>
2020-11-18 03:03:39 +0000

    Moved calculateAge to here and added function to get the current date

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,5 +1,6 @@
 #include "utils.h"
 #include "exceptions.h"
+#include <ctime>
 
 using namespace std;
 
@@ -10,11 +11,11 @@ using namespace std;
  * @param month the date's month
  * @param year the date's year
  */
-Date::Date(unsigned int day, unsigned int month, unsigned int year){
-    if(day > 31)
-        throw InvalidDate(DAY, day);
-    if(month > 12)
+Date::Date(unsigned int day, unsigned int month, unsigned int year) {
+    if (month > 12)
         throw InvalidDate(MONTH, month);
+    if (day > 31)
+        throw InvalidDate(DAY, day);
     this->day = day;
     this->month = month;
     this->year = year;
@@ -27,7 +28,7 @@ Date::Date(unsigned int day, unsigned int month, unsigned int year){
  * @param date the date to be printed
  * @return a reference to the output stream
  */
-std::ostream& operator<< (std::ostream& out, const Date& date) {
+std::ostream &operator<<(std::ostream &out, const Date &date) {
     out << date.day << "/" << date.month << "/" << date.year;
     return out;
 }
@@ -38,10 +39,35 @@ std::ostream& operator<< (std::ostream& out, const Date& date) {
  * @param str the input string
  * @return the input string with all of its characters in uppercase
  */
-string strToUpper(const string& str) {
+string strToUpper(const string &str) {
     string::const_iterator it;
     string ret_str;
     for (it = str.begin(); it != str.end(); it++)
         ret_str.push_back(toupper(*it));
     return ret_str;
+}
+
+/**
+ * Calculate the age of someone born on "birthday", relative to the current day
+ * @param birthday the person's date of birth
+ * @return the age of the person
+ */
+Age calculateAge(const Date &birthday) {
+    time_t current_time;
+    time(&current_time);
+    tm *date = localtime(&current_time);
+    if (date->tm_mon + 1 > birthday.month || date->tm_mon + 1 == birthday.month && date->tm_mday >= birthday.day)
+        return date->tm_year - birthday.year;
+    return date->tm_year - birthday.year - 1;
+}
+
+/**
+ * Gets the current date
+ * @return a Date object with the current date
+ */
+Date getCurrentDate() {
+    time_t current_time;
+    time(&current_time);
+    tm *date = localtime(&current_time);
+    return Date(date->tm_mday, date->tm_mon + 1, date->tm_year + 1900);
 }

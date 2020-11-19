37e4adc7fbca2eb2adc4eaa91684151485b326f1
Lucas Calvet Santos <lucascalvet2000@hotmail.com>
2020-11-19 17:42:57 +0000

    Updated CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
-
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
 
 #include "src/streamz.h"
 #include "src/streamz_framework.h"
 
 int main(int argc, char* argv[]){
-    streamz_framework();
+    //streamz_framework();
 
-    return 0;
+    testing::InitGoogleTest(&argc, argv);
+    std::cout << "-------TESTS STREAMZ-------" << std::endl;
+    return RUN_ALL_TESTS();
 }
